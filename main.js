b3b2288c7f9d506430eabe11373fc1e463cd7117
guilherme <144943817+barbieriguilherme1@users.noreply.github.com>
2025-04-15 10:58:31 -0300

    a12

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2027-8-05T00:00:00");
+const tempoObjetivo1 = new Date("2027-08-05T00:00:00");
 const tempoObjetivo2 = new Date("2026-10-05T00:00:00");
 const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2026-12-01T00:00:00");
